4b1ed73cf0cc651390bf62a1706e735c8ac9e9e7
hyeonnit <lhyeonnit@gmail.com>
2024-04-13 16:49:07 +0900

    accountService getAccountList

--- src/main/java/com/example/bank/account/AccountService.java
+++ src/main/java/com/example/bank/account/AccountService.java
@@ -3,9 +3,15 @@ package com.example.bank.account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class AccountService {
     private final AccountRepository accountRepository;
 
+    public AccountResponse.MainDTO getAccountList(int userId){
+        List<Account> accountList = accountRepository.findByUserId(userId);
+        return new AccountResponse.MainDTO(accountList);
+    }
 }
