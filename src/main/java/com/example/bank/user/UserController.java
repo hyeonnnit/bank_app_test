6c3702aa7e6604b750a8e85a6cdbac25e90bd91b
hyeonnit <lhyeonnit@gmail.com>
2024-04-13 16:08:58 +0900

    userController login

--- src/main/java/com/example/bank/user/UserController.java
+++ src/main/java/com/example/bank/user/UserController.java
@@ -1,4 +1,24 @@
 package com.example.bank.user;
 
+import com.example.bank._core.utils.ApiUtil;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class UserController {
+    private final UserService userService;
+    private final HttpSession session;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserRequest.LoginDTO reqDTO) {
+        String jwt = userService.login(reqDTO.getUsername(), reqDTO.getPassword());
+        return ResponseEntity.ok()
+                .header("Authorization", "Bearer " + jwt)
+                .body(new ApiUtil(null));
+    }
 }
