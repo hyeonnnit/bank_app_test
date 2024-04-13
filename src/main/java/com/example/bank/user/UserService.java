623e7b5d5e6b0156e866aa1ced165b665296f5a3
hyeonnit <lhyeonnit@gmail.com>
2024-04-13 16:02:47 +0900

    userService login

--- src/main/java/com/example/bank/user/UserService.java
+++ src/main/java/com/example/bank/user/UserService.java
@@ -1,4 +1,19 @@
 package com.example.bank.user;
 
+import com.example.bank._core.errors.exception.Exception401;
+import com.example.bank._core.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public String login(String username, String password) {
+        User user = userRepository.findByUsernameAndPassword(username, password)
+                .orElseThrow(()-> new Exception401("username or password incorrect"));
+        String jwt = JwtUtil.create(user);
+        return jwt;
+    }
 }
