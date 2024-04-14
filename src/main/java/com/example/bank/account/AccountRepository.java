03c2b40ff60ba089c8e9470cef9be99e839762d1
hyeonnit <lhyeonnit@gmail.com>
2024-04-14 14:12:52 +0900

    AccountResponse DetailDTO

--- src/main/java/com/example/bank/account/AccountRepository.java
+++ src/main/java/com/example/bank/account/AccountRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    @Query("SELECT ac FROM Account ac JOIN FETCH ac.user u WHERE ac.number = :number")
+    Optional<Account> findByNumber(@Param("number") Integer number);
+
     @Query("SELECT ac FROM Account ac JOIN FETCH ac.user u WHERE ac.user.id = :userId")
     List<Account> findByUserId(@Param("userId") Integer userId);
 }
