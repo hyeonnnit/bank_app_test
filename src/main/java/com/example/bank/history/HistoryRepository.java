8725a313cdf6e3badf46ab5721a019ad5e9d5fae
hyeonnit <lhyeonnit@gmail.com>
2024-04-14 14:25:28 +0900

    HistoryRepository findBySenderOrReceiver

--- src/main/java/com/example/bank/history/HistoryRepository.java
+++ src/main/java/com/example/bank/history/HistoryRepository.java
@@ -1,6 +1,20 @@
 package com.example.bank.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface HistoryRepository extends JpaRepository<History, Integer> {
+
+    @Query("""
+            SELECT h 
+            FROM History h 
+            JOIN FETCH h.sender s 
+            JOIN FETCH h.receiver 
+            WHERE h.sender.number =:number 
+            OR h.receiver.number = :number
+            """)
+    List<History> findBySenderOrReceiver(@Param("number") int number);
 }
